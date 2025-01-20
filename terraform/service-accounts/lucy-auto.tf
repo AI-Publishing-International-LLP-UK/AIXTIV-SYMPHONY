ff65d342f80e2990262f9f54ac14993b1cfa19f8
Phillip Corey Roark <pr@coaching2100.com>
2025-01-20 14:59:06 -0600

    Update Lucy Auto service account permissions

--- terraform/service-accounts/lucy-auto.tf
+++ terraform/service-accounts/lucy-auto.tf
@@ -4,10 +4,18 @@ resource "google_service_account" "lucy_auto" {
   project      = "api-for-warp-drive"
 }
 
-resource "google_project_iam_member" "lucy_auto_ai_platform" {
+resource "google_project_iam_member" "lucy_auto_permissions" {
+  for_each = toset([
+    "roles/aiplatform.user",
+    "roles/run.invoker",
+    "roles/cloudbuild.builds.editor",
+    "roles/storage.objectViewer",
+    "roles/cloudtrace.agent"
+  ])
+  
   project = "api-for-warp-drive"
-  role    = "roles/aiplatform.user"
-  member  = "serviceAccount:lucy-auto@api-for-warp-drive.iam.gserviceaccount.com"
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.lucy_auto.email}"
 }
 
 resource "google_service_account_iam_binding" "admin_account_iam" {
@@ -16,4 +24,8 @@ resource "google_service_account_iam_binding" "admin_account_iam" {
   members = [
     "user:pr@coaching2100.com"
   ]
+}
+
+output "lucy_auto_email" {
+  value = google_service_account.lucy_auto.email
 }
